15a41d20111830a6c264d80a144cdef9ca5658d6
Mark Sena <msena505+git@gmail.com>
2015-01-22 23:22:13 -0800

    set up project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='athospy',
+      version='0.1',
+      description='Athos EMG activity recognition',
+      url='https://bitbucket.org/msena/athospy',
+      author='Mark Sena',
+      author_email='msena505@gmail.com',
+      license='',
+      packages=[''],
+      zip_safe=False)
