7074b9eb76596c8c217a072ed6f25b1e534a5fe0
Mark Sena <msena505+git@gmail.com>
2015-01-28 20:07:45 -0800

    add file summary statistics

--- athospy/plt_config.py
+++ athospy/plt_config.py
@@ -5,8 +5,8 @@ def mpl_plt():
 	plt.style.use('ggplot')
 	
 	mpl.rc('figure', figsize=[16, 9])
-	mpl.rc('xtick', labelsize=20) 
-	mpl.rc('ytick', labelsize=20)
-	mpl.rc('font', size=15)
+	mpl.rc('xtick', labelsize=12) 
+	mpl.rc('ytick', labelsize=12)
+	mpl.rc('font', size=12)
 
 	return mpl, plt
