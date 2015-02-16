fix feature calculation bug
was iterating through a dictionary, which is unsorted, so my features
were getting scrambled

athospy/calcs.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import pandas as pd
 import numpy as np
 from scipy import stats
@@ -20,7 +22,7 @@ def fft_df(df):
         fft_out.append(fft)
 
         freq = np.fft.rfftfreq(len_sig,
-            d=1. / fs)
+                               d=1. / fs)
 
     fft_out = np.array(fft_out).T
 
@@ -57,13 +59,13 @@ def phase_df(df, isplot=False):
     return -dt[xcorr.argmax(axis=0)]
 
 
-def calc_features(df_dict, fnames):
-
+def calc_features(data_dict, keys, standardize=False):
+    print 'Deprecated. Use top_fcns.get_features()'
     freq = []
     peaks = []
     phase = []
-    for fn in fnames:
-        df = df_dict[fn]
+    for key in keys:
+        df = data_dict[key]
 
         _, _, fc = fft_df(df)
         freq.append(fc[::-1])
@@ -77,13 +79,14 @@ def calc_features(df_dict, fnames):
     columns = peak_cols + ['f1', 'f2'] + phase_cols
 
     arr = np.concatenate((peaks, freq, phase), axis=1)
-    df_out = pd.DataFrame(arr, index=fnames,
-        columns=columns)
+    feat = pd.DataFrame(arr, index=keys, columns=columns)
 
-    return df_out
+    if standardize:
+        feat = (feat - feat.mean()) / feat.std()
+
+    return feat
 
 
-# quality control
 def quality(csv_path):
     '''Calculate basic quality metrics for csv file.
     * number of rows - to find files that are too short or long enough to sample multiple times
@@ -93,10 +96,10 @@ def quality(csv_path):
     * max fraction of zero values across channels - possibly dead sensors?
     * max fraction of consecutive non-zero values - files with temporal binning issues
     '''
-    df=fop.load_emg(csv_path)
-    len_df=len(df)
+    df = fop.load_emg(csv_path)
+    len_df = len(df)
 
-    quality={
+    quality = {
         "Length": len_df,
         "Max": df[df < 15000].unstack().max(),
         "Median": df.unstack().median(),

athospy/top_fcns.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import os
 import pandas as pd
 import fnmatch
@@ -9,6 +11,10 @@ import numpy as np
 import visualization as viz
 import calcs as clc
 import fileops as fop
+
+# ML
+from sklearn import svm, metrics
+
 
 
 def label_folders(basepath, write_dst):
@@ -146,31 +152,15 @@ def split_by_personid(files, frac_apprx):
     return files_left, files_right
 
 
-def sample_data(files, n_sec):
-    '''Create dictionary of sampled data, with file ids as keys.
+def get_features(files, n_sec, standardize=False):
+    '''Sample data, calculate features, and collapse into a data frame.
     '''
-    # TODO: allow input of single series?
-    n_samp = int(41.7 * n_sec)
+    index = files.index
+    data_dict = fop.sample_data(files, n_sec)
 
-    data_dict = {}
-    ix__path = zip(files.index, files.Path)
-    for ix, csv_path in ix__path:
-        df = fop.load_emg(csv_path)
-
-        i_mid = len(df) // 2
-        i_start = i_mid - n_samp // 2
-        i_end = i_mid + n_samp // 2
-
-        data_dict[ix] = df.iloc[i_start:i_end]
-    
-    return data_dict
-
-
-def get_features(data_dict, standardize=True):
-    '''id instead of fnames...'''
     freq, peaks, phase = [], [], []
-    for fid in data_dict.iterkeys():
-        df = data_dict[fid]
+    for ix in index:
+        df = data_dict[ix]
 
         _, _, fc = clc.fft_df(df)
         freq.append(fc[::-1])
@@ -184,13 +174,30 @@ def get_features(data_dict, standardize=True):
     columns = peak_cols + ['f1', 'f2'] + phase_cols
 
     arr = np.concatenate((peaks, freq, phase), axis=1)
-    f_df = pd.DataFrame(arr,
-                        index=data_dict.keys(),
-                        columns = columns)
-    if standardize:
-        f_df = (f_df - f_df.mean()) / f_df.std()
+    feat = pd.DataFrame(arr, index=index, columns=columns)
 
-    return f_df
+    if standardize:
+        feat = (feat - feat.mean()) / feat.std()
+
+    return feat
+
+
+def prediction_report(predicted, labels, classes, plot_on=True):
+
+    avg_correct = sum(predicted==labels) / len(predicted) * 100
+    print '\npercent correct:', avg_correct
+
+    counts = labels.groupby(labels.values).count().values
+    mat = metrics.confusion_matrix(labels, predicted)
+
+    print mat
+    print metrics.classification_report(labels, predicted)
+
+    frac_predicted = (mat.T / counts).T
+    if plot_on:
+        viz.plot_confusion(frac_predicted, classes)
+
+    return avg_correct
 
 # Helper functions
 ###################################################